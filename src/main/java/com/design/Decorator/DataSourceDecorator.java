6f19d257f0dbf481a7f5c140d6a9e09c9138193e
Ivan <vanysemka@gmail.com>
2022-11-06 22:12:32 +0300

    Created Decorator pattern example

---
+++ src/main/java/com/design/Decorator/DataSourceDecorator.java
@@ -0,0 +1,20 @@
+package com.design.Decorator;
+
+public abstract class DataSourceDecorator implements DataSource {
+
+    private DataSource wrappee;
+
+    public DataSourceDecorator(DataSource source) {
+        this.wrappee = source;
+    }
+
+    @Override
+    public void writeData(String data) {
+        wrappee.writeData(data);
+    }
+
+    @Override
+    public String readData() {
+        return wrappee.readData();
+    }
+}
